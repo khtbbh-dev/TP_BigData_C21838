c47c7a68bd961cce68b6365c0da11a83b4ad091b
khtbbh-dev <elkheitbbh@gmail.com>
2026-06-14 18:33:03 +0000

    Update bigdata_manager.sh

--- hadoop-hdfs/Big_DataManager/bigdata_manager.sh
+++ hadoop-hdfs/Big_DataManager/bigdata_manager.sh
@@ -5,63 +5,34 @@ do
 
 clear
 
-echo "========================================"
-echo "      BIG DATA MANAGER    "
-echo "========================================"
-echo ""
-echo "1 - Cluster Manager"
-echo "2 - HDFS Manager"
-echo "3 - MapReduce Manager"
-echo "4 - YARN Manager"
-echo "5 - Spark Manager"
-echo "6 - Hive Manager"
-echo "7 - Reports Manager"
-echo ""
+echo "======================================="
+echo " BIG DATA MANAGER "
+echo "======================================="
+
+echo "1 - Gestion HDFS"
+echo "2 - Gestion MapReduce"
+echo "3 - Gestion Spark"
+echo "4 - Gestion Hive"
+echo "5 - Gestion Cluster"
+echo "6 - Rapports"
+
 echo "0 - Quitter"
-echo ""
 
 read -p "Choix : " CH
 
 case $CH in
 
-1)
-    source modules/cluster.sh
-    ;;
+1) bash modules/hdfs.sh ;;
+2) bash modules/mapreduce.sh ;;
+3) bash modules/spark.sh ;;
+4) bash modules/hive.sh ;;
+5) bash modules/cluster.sh ;;
+6) bash modules/reports.sh ;;
 
-2)
-    source modules/hdfs.sh
-    ;;
+0) exit ;;
 
-3)
-    source modules/mapreduce.sh
-    ;;
-
-4)
-    source modules/yarn.sh
-    ;;
-
-5)
-    source modules/spark.sh
-    ;;
-
-6)
-    source modules/hive.sh
-    ;;
-
-7)
-    source modules/reports.sh
-    ;;
-
-0)
-    exit
-    ;;
-
-*)
-    echo "Choix invalide"
-    sleep 2
-    ;;
-
+*) echo "Choix invalide" ;;
 esac
 
+read -p "Entrée pour continuer..."
 done
-```
